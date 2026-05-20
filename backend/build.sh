2c63c441a6de314b7fa4292262c1ce33e5768a32
Md Sium <siamhossain1286@gmail.com>
2026-05-20 15:53:47 +0600

    Add automatic superuser creation

---
+++ backend/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
+python manage.py migrate
+
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        'admin',
+        'admin@gmail.com',
+        '1234'
+    )
+END
